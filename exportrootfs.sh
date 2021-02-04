17dda88c962ac68037f11c26236ae4bb50d9dd6b
Izaak Lauer <izaak@izaaklauer.com>
2021-02-04 16:53:54 -0500

    Working now

--- exportrootfs.sh
+++ exportrootfs.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -x 
 
-name=
-docker build . -t 
+project_name=${PWD##*/}
+container=$(echo ${project_name}:$(git rev-parse --short HEAD))
+
+export_path=${1:-${project_name}.tar}
+
+docker build . -t $container
+docker export $(docker create $container) -o $export_path
+
+echo "exported to $export_path"
